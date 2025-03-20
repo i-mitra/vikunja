d8293f2c6821858f876064ea7b71437e75677d1d
Ishan Mitra <imitra.pro@gmail.com>
2025-03-20 16:02:53 -0400

    Updated system prompt to return list of element object

--- frontend/src/components/misc/DropoutChat.config.js
+++ frontend/src/components/misc/DropoutChat.config.js
@@ -1,11 +1,13 @@
-export const system_prompt = `From the following code identify the the elements the user is trying to identify. Return the elements that needs to be highlighted in json format that contains the element's unique set of sttributes, tag and textContent.
+export const system_prompt = `From the following code identify the the elements the user is trying to identify. Return the elements that need to be highlighted, in json format that contains the element's tag, textContent and unique set of attributes, which should be in a dictionary.
 
-Use the following example as the format the highlighted element must be returned as.
-{
-    attributes: [
+Use the following example as the format for an element that has class and href as unique attribute set. As the output only return the JSON data that must be in the following format. Always return as a JSON list, even when singular element is returned.
+[
+  {
+    attributes: {
         class: "main-container",
         href: "/link/to/something",
-    ],
+    },
     tag: "a",
     textContent: "Lorem Epsum",
-}`
+  }
+]`
