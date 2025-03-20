77ce37a85f46558ab5ca031b329f9aafd69b93dd
Ishan Mitra <imitra.pro@gmail.com>
2025-03-20 16:21:48 -0400

    fixed attributes being a list

--- frontend/src/components/misc/DropoutChat.config.js
+++ frontend/src/components/misc/DropoutChat.config.js
@@ -1,4 +1,4 @@
-export const system_prompt = `From the following code identify the the elements the user is trying to identify. Return the elements that need to be highlighted, in json format that contains the element's tag, textContent and unique set of attributes, which should be in a dictionary.
+export const system_prompt = `From the following code identify the the elements the user is trying to identify. Return the elements that need to be highlighted, in json format that contains the element's tag, textContent and unique set of attributes, which must be a dictionary and not a list of objects.
 
 Use the following example as the format for an element that has class and href as unique attribute set. As the output only return the JSON data that must be in the following format. Always return as a JSON list, even when singular element is returned.
 [
