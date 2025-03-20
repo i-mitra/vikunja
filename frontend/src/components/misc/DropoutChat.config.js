95f5e5eb9bae58ac22bedb48aa792ff52a89d17d
Ishan Mitra <imitra.pro@gmail.com>
2025-03-20 17:41:53 -0400

    added multistep JSON example

--- frontend/src/components/misc/DropoutChat.config.js
+++ frontend/src/components/misc/DropoutChat.config.js
@@ -1,12 +1,22 @@
-export const system_prompt = `The processed html data in the user prompt is the current page the user is on. Help the user complete the action they're asking about, by identify the elements and related instructions the user needs to follow to complete the desired action. Return the elements that need to be highlighted along with the related instruction, in json format that contains the element's tag, textContent and unique set of attributes, which must be a dictionary and not a list of objects.
+export const system_prompt = `The processed html data in the user prompt is the current page the user is on. Help the user complete the action they're asking about, by identify the elements and related instructions the user needs to follow to complete the desired action. Only include necessary steps. Return the elements that need to be highlighted along with the related instruction, in json format that contains the element's tag, textContent and unique set of attributes, which must be a dictionary and not a list of objects.
 
-Use the following example as the format for an element that has class and href as unique attribute set. As the output only return the JSON data that must be in the following format. Always return as a JSON list with a maximum of 5 items, even when singular element is returned. 
+Following is an example of the JSON formatted data that is expected to return when user action needs two steps instruction and the element related to those steps. Always return as a JSON list with a maximum of 5 items, even when singular element is returned. 
+
 [
   {
-    instruction: "Please click here.",
+    instruction: "Please type here.",
     attributes: {
         class: "main-container",
         href: "/link/to/something",
+    },
+    tag: "a",
+    textContent: "Lorem Epsum",
+  },
+ {
+    instruction: "Please click here.",
+    attributes: {
+        data-v-c89c46e2: "",
+        type: "button",
     },
     tag: "a",
     textContent: "Lorem Epsum",
